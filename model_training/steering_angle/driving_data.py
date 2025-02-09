7269d8fde7b450bca2958344883ca7a2c4da66e2
Ayan Gairola <ayangairola@Ayans-MacBook-Pro.local>
2025-02-10 01:22:43 +0530

    feat: add training logic for steering angle model, fix : syntax error in model.py and driving_data.py

--- model_training/steering_angle/driving_data.py
+++ model_training/steering_angle/driving_data.py
@@ -1,35 +1,39 @@
 import cv2
+import os
 import random
 import numpy as np
 
 xs = []
 ys = []
 
-#points to the end of the last batch
+# Points to the end of the last batch
 train_batch_pointer = 0
 val_batch_pointer = 0
 
-#reading data
-with open('/data/driving_dataset/data.txt') as f:
+# Base path for dataset
+base_path = '../../data/driving_dataset/'
+
+# Reading data
+with open(os.path.join(base_path, 'data.txt')) as f:
     for line in f:
-        xs.append('/data/driving_dataset/') + line.split()[0]
+        image_path = os.path.join(base_path, line.split()[0])
+        xs.append(image_path)
         '''
-        
-        the paper by nvidia uses inv of turning radius, but the steering wheel angle is proportional to the inv of turning radius so the steering wheel angle in output is used as radians(pi/180).
-        
+        The paper by NVIDIA uses the inverse of turning radius, 
+        but the steering wheel angle is proportional to the inverse of turning radius.
+        So, the steering wheel angle in the output is used as radians (pi/180).
         '''
         ys.append(float(line.split()[1]) * 3.14159265 / 180)
-        
 
 num_images = len(xs)
 
-# shuffling list of images
-c = list(zip(xs,ys))
+# Shuffling list of images
+c = list(zip(xs, ys))
 random.shuffle(c)
 xs, ys = zip(*c)
 
-train_xs = xs[:int(len(xs)*0.8)]
-train_ys = ys[:int(len(ys)*0.8)]
+train_xs = xs[:int(len(xs) * 0.8)]
+train_ys = ys[:int(len(ys) * 0.8)]
 
 val_xs = xs[-int(len(xs) * 0.2):]
 val_ys = ys[-int(len(xs) * 0.2):]
@@ -37,26 +41,46 @@ val_ys = ys[-int(len(xs) * 0.2):]
 num_train_images = len(train_xs)
 num_val_images = len(val_xs)
 
+
 def LoadTrainBatch(batch_size):
     global train_batch_pointer
     x_out = []
     y_out = []
+
     for i in range(batch_size):
-        x_out.append(cv2.resize(cv2.imread(train_xs[(train_batch_pointer + i) % num_train_images])[-150:], (200,66))/255.0)
+        image_path = train_xs[(train_batch_pointer + i) % num_train_images]
+        img = cv2.imread(image_path)
+
+        if img is None:
+            print(f"Skipping missing image: {image_path}")
+            continue  # Skip this image
+
+        img = img[-150:]  # Crop the last 150 pixels
+        img = cv2.resize(img, (200, 66)) / 255.0
+        x_out.append(img)
         y_out.append([train_ys[(train_batch_pointer + i) % num_train_images]])
-    
+
     train_batch_pointer += batch_size
     return x_out, y_out
+
 
 def LoadValBatch(batch_size):
     global val_batch_pointer
     x_out = []
     y_out = []
-    for i in range(0, batch_size):
-        x_out.append(cv2.resize(cv2.imread(val_xs[(val_batch_pointer + i) % num_val_images])[-150:], (200, 66)) / 255.0)
+
+    for i in range(batch_size):
+        image_path = val_xs[(val_batch_pointer + i) % num_val_images]
+        img = cv2.imread(image_path)
+
+        if img is None:
+            print(f"Skipping missing validation image: {image_path}")
+            continue  # Skip this image
+
+        img = img[-150:]
+        img = cv2.resize(img, (200, 66)) / 255.0
+        x_out.append(img)
         y_out.append([val_ys[(val_batch_pointer + i) % num_val_images]])
-        
+
     val_batch_pointer += batch_size
     return x_out, y_out
-
-
--- model_training/steering_angle/model.py
+++ model_training/steering_angle/model.py
@@ -2,47 +2,52 @@ import tensorflow.compat.v1 as tf
 tf.disable_v2_behavior()
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape, stddev=0.1)
-    return tf.Variable(initial)
+  initial = tf.truncated_normal(shape, stddev=0.1)
+  return tf.Variable(initial)
 
 def bias_variable(shape):
-    initial = tf.constant(0.1, shape=shape)
-    return tf.Variable(initial)
+  initial = tf.constant(0.1, shape=shape)
+  return tf.Variable(initial)
 
 def conv2d(x, W, stride):
-    return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='VALID')
+  return tf.nn.conv2d(x, W, strides=[1, stride, stride, 1], padding='VALID')
 
 x = tf.placeholder(tf.float32, shape=[None, 66, 200, 3])
-y = tf.placeholder(tf.float32, shape=[None, 1])
+y_ = tf.placeholder(tf.float32, shape=[None, 1])
 
 x_image = x
 
-#First conv layer
-W_conv1 = weight_variable([5,5,3,24])
+#first convolutional layer
+W_conv1 = weight_variable([5, 5, 3, 24])
 b_conv1 = bias_variable([24])
-h_conv1 = tf.nn.relu(conv2d(x_image,W_conv1,2) + b_conv1)
 
-#Second conv layer
-W_conv2 = weight_variable([5,5,24,36])
+h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1, 2) + b_conv1)
+
+#second convolutional layer
+W_conv2 = weight_variable([5, 5, 24, 36])
 b_conv2 = bias_variable([36])
-h_conv2 = tf.nn.relu(conv2d(h_conv1,W_conv2,2) + b_conv2)
 
-#Third conv layer
-W_conv3 = weight_variable([5,5,36,48])
+h_conv2 = tf.nn.relu(conv2d(h_conv1, W_conv2, 2) + b_conv2)
+
+#third convolutional layer
+W_conv3 = weight_variable([5, 5, 36, 48])
 b_conv3 = bias_variable([48])
-h_conv3 = tf.nn.relu(conv2d(h_conv2,W_conv3,2) + b_conv3)
 
-#Fourth conv layer
-W_conv4 = weight_variable([5,5,48,64])
+h_conv3 = tf.nn.relu(conv2d(h_conv2, W_conv3, 2) + b_conv3)
+
+#fourth convolutional layer
+W_conv4 = weight_variable([3, 3, 48, 64])
 b_conv4 = bias_variable([64])
-h_conv4 = tf.nn.relu(conv2d(h_conv3,W_conv4,1) + b_conv4)
 
-#Fifth conv layer
-W_conv5 = weight_variable([5,5,64,64])
+h_conv4 = tf.nn.relu(conv2d(h_conv3, W_conv4, 1) + b_conv4)
+
+#fifth convolutional layer
+W_conv5 = weight_variable([3, 3, 64, 64])
 b_conv5 = bias_variable([64])
-h_conv5 = tf.nn.relu(conv2d(h_conv4,W_conv5,1) + b_conv5)
 
-#Dense layer 1 (fcl)
+h_conv5 = tf.nn.relu(conv2d(h_conv4, W_conv5, 1) + b_conv5)
+
+#FCL 1
 W_fc1 = weight_variable([1152, 1164])
 b_fc1 = bias_variable([1164])
 
@@ -80,8 +85,4 @@ h_fc4_drop = tf.nn.dropout(h_fc4, keep_prob)
 W_fc5 = weight_variable([10, 1])
 b_fc5 = bias_variable([1])
 
-#scale the atan output
-y = tf.multiply(tf.atan(tf.matmul(h_fc4_drop, W_fc5) + b_fc5), 2)
-
-
-
+y = tf.multiply(tf.atan(tf.matmul(h_fc4_drop, W_fc5) + b_fc5), 2) #scale the atan output
